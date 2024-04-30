c96392c0c28fa40d986d74c2f4d98be241a57652
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-04-30 14:27:27 +0700

    6. User Input

--- lib/component/ship.dart
+++ lib/component/ship.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
@@ -7,7 +8,14 @@ class Ship extends SpriteComponent {
   @override
   FutureOr<void> onLoad() async {
     sprite = Sprite(await Flame.images.load("ships/spaceShips_001.png"));
-    position = Vector2(0, 0);
+    position = Vector2(100, 150);
+    angle = -pi / 2;
+    scale = Vector2(2.5, 1);
     anchor = Anchor.center;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
   }
 }
