1a2130db267ec4398927a027752c42d5efeaf084
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-05-28 08:45:28 +0700

    7. Input

--- lib/component/ship.dart
+++ lib/component/ship.dart
@@ -2,15 +2,15 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 
 class Ship extends SpriteComponent {
   @override
   FutureOr<void> onLoad() async {
     sprite = Sprite(await Flame.images.load("ships/spaceShips_001.png"));
-    position = Vector2(100, 150);
+    position = Vector2(150, 150);
     angle = -pi / 2;
-    scale = Vector2(2.5, 1);
     anchor = Anchor.center;
   }
 
--- lib/pages/my_flame_game.dart
+++ lib/pages/my_flame_game.dart
@@ -1,13 +1,31 @@
 import 'dart:async';
+import 'dart:developer';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_udemy_class/component/ship.dart';
+import 'package:flutter/src/services/hardware_keyboard.dart';
+import 'package:flutter/src/services/keyboard_key.g.dart';
+import 'package:flutter/src/widgets/focus_manager.dart';
 
-class MyFlameGame extends FlameGame {
+class MyFlameGame extends FlameGame with PanDetector {
+  late Ship s;
+
   @override
   FutureOr<void> onLoad() async {
-    Ship s = Ship();
+    s = Ship();
     add(s);
 
     //add(SpriteComponent());
   }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    s.position = info.eventPosition.global;
+  }
+
+  // @override
+  // void onTapDown(TapDownEvent event) {
+  //   log("TapDownEvent ${event.localPosition}");
+  //   s.position.add(Vector2(10, 0));
+  // }
 }
