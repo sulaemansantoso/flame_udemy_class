75101da9517c2a464fb17e468bd2d8179ff64964
Sulaeman Santoso <sulaeman.santoso@gmail.com>
2024-07-09 15:14:00 +0700

    updated

--- README.md
+++
@@ -1,3 +0,0 @@
-# flame_udemy_class
-
-A new Flutter project.
--- android/app/src/main/kotlin/com/example/flame_udemy_class/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flame_udemy_class
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
